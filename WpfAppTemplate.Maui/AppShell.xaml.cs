5f04b51deb19d26d351509b50c32b096bde9e1cb
Kitfka <philipthuys@gmail.com>
2023-06-28 11:45:15 +0200

    Add Maui

---
+++ WpfAppTemplate.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WpfAppTemplate.Maui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ WpfAppTemplate.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace WpfAppTemplate.Maui;
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
